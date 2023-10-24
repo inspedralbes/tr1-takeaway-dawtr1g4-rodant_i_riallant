072b1f50030ce1322c388cebadcf9f29e20fe245
Arnau-fg <a22arnfergil@inspedralbes.cat>
2023-10-24 09:18:24 +0200

    Creat api

--- Content/Back/api-laravel/routes/api.php
+++ Content/Back/api-laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\producteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/productes', [producteController::class, 'index']);
+
+Route::post('/productes', [producteController::class, 'store']);
+
+Route::get('/productes/{id}', [producteController::class, 'show']);
+
+Route::patch('/productes/{id}', [producteController::class, 'update']);
+
+Route::delete('/productes/{id}', [producteController::class, 'destroy']);
+
+Route::get('/productes/search/{name}', [producteController::class, 'search']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
