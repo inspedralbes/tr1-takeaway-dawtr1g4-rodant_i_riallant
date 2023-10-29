bc8de450c4ae9fe28a54b8c9731ebca38e674f6c
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2023-10-29 22:23:25 +0100

    Merge Develop on main : Success confirmed

--- Content/Front/ComunicationManager.js
+++ Content/Front/ComunicationManager.js
@@ -1,5 +1,5 @@
 export async function agafarPelicules(){
-    const response = await fetch(`http://preprod.rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/productes`);
+    const response = await fetch(`http://rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/productes`);
     //http://rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/productes
     //http://.preprod.rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/productes
     //http://localhost:8000/api/productes
@@ -12,14 +12,14 @@ export async function agafarPelicules(){
 }
 
 export async function agafarCategories(){
-    const response = await fetch(`http://preprod.rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/categories`);
+    const response = await fetch(`http://rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/categories`);
     const categories = await response.json();
 
     return categories;
 }
 
 export async function enviarComanda(objecte){
-    const url = 'http://preprod.rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/comanda';
+    const url = 'http://rirtr1g4.daw.inspedralbes.cat/Back/api-laravel/public/api/comanda';
 
                 // Datos que deseas enviar en formato de formulario
                 const formData = new URLSearchParams();
