c0a5f570c6d5da4edf49e92013b672da2a7dcc5f
Arnau-fg <a22arnfergil@inspedralbes.cat>
2023-11-03 09:13:47 +0100

    Corregit majúscules per a prod

--- Content/Back/api-laravel/app/Http/Controllers/categoriaController.php
+++ Content/Back/api-laravel/app/Http/Controllers/categoriaController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\categoria;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class categoriaController extends Controller
 {
     public function index(){
-        return categoria::all();
+        return Categoria::all();
     }
 
 }
